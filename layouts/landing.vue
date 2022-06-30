63026aec3a8cf69d75fe4f7fab94202e0f55825b
joni <mesfinjohn@rocketmail.com>
2022-06-30 16:42:25 +0300

    error on plugins/vdf-plugins.ts

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>
-        <Navbar/>
-        <slot/>
-    </div>
+  <div @scroll="handleScroll">
+    <Navbar />
+    <slot />
+  </div>
 </template>
 
 <script setup>
+onMounted(() => window.addEventListener("scroll", handleScroll));
 
+const handleScroll = () => {
+  console.log("helo scroll");
+};
+
+// const scrollClass = ref("");
+
+// const scrollFunc = () => {
+//   scrollClass.value = "shadow-none hover:shadow-opaq duration-300";
+//   console.log('hello');
+// };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
@@ -9,7 +9,12 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+        "opaq": "0 4px 16px 6px rgba(0,0,0,0.4)",
+      },
+    },
   },
   plugins: [],
 };
